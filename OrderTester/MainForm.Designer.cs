8bf8277ea805bd0ff0586c4ff9d9dfc0f87f8f57
강동훈 <anthony_10@daum.net>
2025-02-02 23:51:46 +0900

    초기 커밋.

---
+++ OrderTester/MainForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace OrderTester
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOrderId = new System.Windows.Forms.TextBox();
+            this.txtMenuItem = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmitOrder = new System.Windows.Forms.Button();
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtOrderId
+            // 
+            this.txtOrderId.Location = new System.Drawing.Point(132, 69);
+            this.txtOrderId.Name = "txtOrderId";
+            this.txtOrderId.Size = new System.Drawing.Size(100, 21);
+            this.txtOrderId.TabIndex = 0;
+            // 
+            // txtMenuItem
+            // 
+            this.txtMenuItem.Location = new System.Drawing.Point(132, 113);
+            this.txtMenuItem.Name = "txtMenuItem";
+            this.txtMenuItem.Size = new System.Drawing.Size(243, 21);
+            this.txtMenuItem.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OrderId";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "MenuItem";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Quantity";
+            // 
+            // btnSubmitOrder
+            // 
+            this.btnSubmitOrder.Location = new System.Drawing.Point(382, 253);
+            this.btnSubmitOrder.Name = "btnSubmitOrder";
+            this.btnSubmitOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmitOrder.TabIndex = 7;
+            this.btnSubmitOrder.Text = "주문 생성";
+            this.btnSubmitOrder.UseVisualStyleBackColor = true;
+            this.btnSubmitOrder.Click += new System.EventHandler(this.BtnSubmitOrder_ClickAsync);
+          
+            // 
+            // numQuantity
+            // 
+            this.numQuantity.Location = new System.Drawing.Point(132, 157);
+            this.numQuantity.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numQuantity.Name = "numQuantity";
+            this.numQuantity.Size = new System.Drawing.Size(120, 21);
+            this.numQuantity.TabIndex = 8;
+            this.numQuantity.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 288);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.btnSubmitOrder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMenuItem);
+            this.Controls.Add(this.txtOrderId);
+            this.Name = "MainForm";
+            this.Text = "주문 생성 테스트";
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOrderId;
+        private System.Windows.Forms.TextBox txtMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubmitOrder;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+    }
+}
+
---
+++ OrderTester/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace OrderTester
+{
+    public partial class MainForm : Form
+    {
+        private readonly HttpClient _httpClient;
+        private const string ApiUrl = "http://localhost:5000/api/orders";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+        }
+
+        private async void BtnSubmitOrder_ClickAsync(object sender, EventArgs e)
+        {
+            var order = new Order
+            {
+                OrderId = txtOrderId.Text,
+                MenuItem = txtMenuItem.Text,
+                Quantity = (int)numQuantity.Value,
+                Status = "대기 중"
+            };
+
+            string json = JsonSerializer.Serialize(order);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await _httpClient.PostAsync(ApiUrl, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("주문이 성공적으로 전송되었습니다!", "성공", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("주문 전송 실패! 서버 응답: " + response.StatusCode, "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("서버와의 연결에 실패했습니다!\n" + ex.Message, "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string MenuItem { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+    }
+}
